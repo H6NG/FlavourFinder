951bec26e68dd983f26140eb336d4da64ae3aa0c
H6NG <8.h.a.n.g.l.i.u.8@gmail.com>
2025-11-01 00:35:04 -0400

    A

--- frontend/src/mainPage.jsx
+++ frontend/src/mainPage.jsx
@@ -4,6 +4,15 @@ function ClientPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
       <h1 className="text-7xl font-extrabold text-gray-900">We choose. You enjoy.</h1>
+
+      <div className="flex space-x-4">
+        <button className="px-6 py-3 bg-blue-600 text-white rounded-xl shadow-md hover:bg-blue-700 transition">
+        Sign Up
+        </button>
+        <button className="px-6 py-3 bg-gray-200 text-gray-900 rounded-xl shadow-md hover:bg-gray-300 transition">
+        Login
+        </button>
+      </div>
     </div>
   );
 }
