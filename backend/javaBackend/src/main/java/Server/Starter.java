0dabc031cf43959b081d7878cd0925f011ae646a
Aaron Zhao <aaronyumozhao@hotmail.com>
2025-12-01 15:12:23 -0800

    Updated Schema, added database status check

--- backend/javaBackend/src/main/java/Server/Starter.java
+++ backend/javaBackend/src/main/java/Server/Starter.java
@@ -1,9 +1,12 @@
 package Server;
 
+import db_operations.DBConnection;
+
 public class Starter {
     public static void main(String[] args) {
         Server server = new Server(8000);
         server.startServer();
+        System.out.println(DBConnection.getStatus());
         try {
             Thread.sleep(1000 * 1000);
         } catch (InterruptedException e) {
