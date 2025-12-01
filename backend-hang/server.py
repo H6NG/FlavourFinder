31029ae4df5a498bd4fe508985e8ff77e7a52758
H6NG <8.h.a.n.g.l.i.u.8@gmail.com>
2025-12-01 02:34:34 -0500

    skeleton python

--- backend-hang/server.py
+++ backend-hang/server.py
@@ -6,5 +6,70 @@ from dotenv import load_dotenv
 load_dotenv(); 
 
 app = Flask(__name__); 
+CORS(app); 
 
 @app.route('/')
+def home():
+    return "For DEBUGGING: Flask backend connected to MongoDB securely."
+
+@app.route('/api/refreshToken', methods=['GET'])
+def refreshToken(): 
+
+
+
+
+@app.route('/api/getChoice', methods = ['GET'])
+def getChoice():
+
+
+@app.route('/api/makeChoice', methods = ['POST'])
+def makeChoice():
+
+
+
+@app.route('/api/recommendRestaurauntGuest', methods = ['POST'])
+def recommendRestaurantGuest(): 
+
+
+@app.route('/api/recommendRestaurauntUser', methods = ['POST'])
+def recommendRestaurantUser():
+
+
+@app.route('/api/changePassword', methods = ['PUT'])
+def changePassword():
+
+
+@app.route('/api/deleteAccount', methods = ['DELETE'])
+def deleteAccount():
+
+
+@app.route('/api/getPreferences', methods = ['GET'])
+def getPreferences():
+    
+
+@app.route('/api/getUserSelf', methods = ['GET'])
+def getUserSelf():
+
+
+@app.route('/api/registerUser', methods = ['POST'])
+def registerUser():
+
+
+@app.route('/api/updatePreferences', methods = ['PUT'])
+def updatePreferences():
+
+@app.route('/api/updateUserInfo', methods = ['POST'])
+def updateUserInfo():
+
+
+@app.route('/api/userLogin', methods = ['POST'])
+def userLogin():
+
+
+@app.route('/api/userLogout', methods = ['POST'])
+def userLogout():
+
+
+
+if __name__ == '__main__': 
+    app.run (debug=True)
